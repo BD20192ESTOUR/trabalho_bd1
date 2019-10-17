eac4a58b31ba1921ade36877ed949134b54505f7
Carolina de Souza Viana <carolina.wuu@gmail.com>
2019-10-17 16:29:24 -0300

    Delete CAROLINA_CONSULTA_TOPICO_3_4.sql

--- images/CAROLINA_CONSULTA_TOPICO_3_4.sql
+++
@@ -1,4 +0,0 @@
-
-SELECT P.tipo_pontoturistico,P.nome_pontoturistico,P.publico_alvo,P.tipo_ambiente,concat(P.tipo_logradouro,' ',P.logradouro,' - ',P.numero,' - ',P.bairro,' - Cep: ',P.cep,' - ',P.municipio) AS endereco
- FROM pontoturistico P WHERE P.idpontoturistico IN (select idpontoturistico FROM pontoturisticoavaliacao GROUP BY idpontoturistico HAVING avg(nota) > 6)
- AND (publico_alvo ilike 'amigos' OR publico_alvo ilike 'adultos') AND municipio NOT ilike 'serra';
