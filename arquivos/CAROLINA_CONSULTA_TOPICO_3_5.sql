4809fa5ccbdc7797414ab88a3e52f93ebe554a5a
Carolina de Souza Viana <carolina.wuu@gmail.com>
2019-10-17 16:37:55 -0300

    Update CAROLINA_CONSULTA_TOPICO_3_5.sql

--- arquivos/CAROLINA_CONSULTA_TOPICO_3_5.sql
+++ arquivos/CAROLINA_CONSULTA_TOPICO_3_5.sql
@@ -1,3 +1,4 @@
+/* Ponto Turisticos que não receberam nenhuma avaliação, e publico alvo seja amigos ou o municipio não seja serra */
 SELECT P.tipo_pontoturistico,P.nome_pontoturistico,P.publico_alvo,P.tipo_ambiente,concat(P.tipo_logradouro,' ',P.logradouro,' - ',P.numero,' - ',P.bairro,' - Cep: ',P.cep,' - ',P.municipio) AS endereco
  FROM pontoturistico P WHERE P.idpontoturistico NOT IN (select idpontoturistico FROM pontoturisticoavaliacao GROUP BY idpontoturistico)
  AND (publico_alvo ilike 'amigos' OR municipio NOT ilike 'serra');
