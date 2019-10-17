4aa42a6748dfd4becd23ac5d3e1ca1488f304639
Carolina de Souza Viana <carolina.wuu@gmail.com>
2019-10-17 16:34:34 -0300

    Update CAROLINA_CONSULTA_TOPICO_3_4.sql

--- arquivos/CAROLINA_CONSULTA_TOPICO_3_4.sql
+++ arquivos/CAROLINA_CONSULTA_TOPICO_3_4.sql
@@ -1,3 +1,4 @@
+/* Pontos Turisticos onde média de nota de avaliação é maior que 6, e o publico alvo seja amigos ou adultos, e o municipio não seja serra */
 
 SELECT P.tipo_pontoturistico,P.nome_pontoturistico,P.publico_alvo,P.tipo_ambiente,concat(P.tipo_logradouro,' ',P.logradouro,' - ',P.numero,' - ',P.bairro,' - Cep: ',P.cep,' - ',P.municipio) AS endereco
  FROM pontoturistico P WHERE P.idpontoturistico IN (select idpontoturistico FROM pontoturisticoavaliacao GROUP BY idpontoturistico HAVING avg(nota) > 6)
