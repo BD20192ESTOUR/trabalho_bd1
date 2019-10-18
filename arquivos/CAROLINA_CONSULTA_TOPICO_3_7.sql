ddf2ae704fb877e764151e83ffe36e03af502b66
Carolina de Souza Viana <carolina.wuu@gmail.com>
2019-10-17 21:49:01 -0300

    Update CAROLINA_CONSULTA_TOPICO_3_7.sql

--- arquivos/CAROLINA_CONSULTA_TOPICO_3_7.sql
+++ arquivos/CAROLINA_CONSULTA_TOPICO_3_7.sql
@@ -1,2 +1,5 @@
-/* alterar o nome do campo comentario para avaliacao na tabela ponto turistico */
+/* alterar o nome do campo comentario para avaliacao na tabela pontoturistico */
+
 ALTER TABLE pontoturisticoavaliacao RENAME comentario TO avaliacao;
+
+Select * from pontoturisticoavaliacao limit 2;
