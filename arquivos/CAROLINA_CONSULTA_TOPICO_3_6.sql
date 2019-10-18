878f2c79947e47fe5ea457b0108fe4ad6b4d0374
Carolina de Souza Viana <carolina.wuu@gmail.com>
2019-10-17 21:45:51 -0300

    Update CAROLINA_CONSULTA_TOPICO_3_6.sql

--- arquivos/CAROLINA_CONSULTA_TOPICO_3_6.sql
+++ arquivos/CAROLINA_CONSULTA_TOPICO_3_6.sql
@@ -1,2 +1,6 @@
 /* Alterar o campo nome ponto turistico para nome */
 ALTER TABLE pontoturistico RENAME nome_pontoturistico TO nome;
+
+
+SELECT tipo_pontoturistico,nome,publico_alvo,tipo_ambiente,concat(tipo_logradouro,' ',logradouro,' - ',numero,' - ',bairro,' - Cep: ',cep,' - ',municipio) AS endereco 
+FROM pontoturistico offset 3;
