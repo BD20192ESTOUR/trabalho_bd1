e1c10f8465eab77d3141696f3221d381daa283fc
Alexandre-Jastrow <52933958+Alexandre-Jastrow@users.noreply.github.com>
2019-11-12 19:21:32 -0300

    Update and rename willian_like2.sql to alexandre_like2.sql

--- arquivos/willian_like2.sql
+++
@@ -1,3 +0,0 @@
-Select nome_pontoturistico, evento.descricao_evento,evento.dt_evento, evento.link_evento, publico_alvo, tipo_ambiente from pontoturistico left outer join evento on (pontoturistico.idpontoturistico = evento.idpontoturistico)
-where pontoturistico.tipo_ambiente ilike '%praia%' or pontoturistico.tipo_ambiente ilike '%parque%' or pontoturistico.tipo_ambiente ilike '%farol%' and pontoturistico.municipio ilike '%Vitoria%'
-limit 5
