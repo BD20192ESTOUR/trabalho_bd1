11ae5570702255efddb2907dc2e70b9c4a052dc4
Alexandre-Jastrow <52933958+Alexandre-Jastrow@users.noreply.github.com>
2019-11-12 19:54:51 -0300

    Update and rename willian_alteracao1.sql to alexandre_alteracao_1.sql

---
+++ arquivos/alexandre_alteracao_1.sql
@@ -0,0 +1,1 @@
+update evento set idpontoturistico = 15 where descricao_evento ilike '%corrida maluca%';
--- arquivos/willian_alteracao1.sql
+++
@@ -1,3 +0,0 @@
-update evento
-set idpontoturistico = 15
-where descricao_evento ilike '%corrida maluca%'
