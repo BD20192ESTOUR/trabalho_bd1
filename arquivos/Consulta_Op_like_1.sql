06e25ea3f6a657c38f4b9b8c44c7d262f4ec1337
Carolina de Souza Viana <carolina.wuu@gmail.com>
2019-11-17 19:20:19 -0300

    Update and rename alexandre_like_1.sql to Consulta_Op_like_1.sql

---
+++ arquivos/Consulta_Op_like_1.sql
@@ -0,0 +1,4 @@
+Select nome_pontoturistico, descricao, publico_alvo, tipo_ambiente from pontoturistico 
+WHERE nome_pontoturistico ilike '%Vila Velha%' or
+bairro ilike '%Vila Velha%' or 
+municipio ilike '%Vila Velha%';
