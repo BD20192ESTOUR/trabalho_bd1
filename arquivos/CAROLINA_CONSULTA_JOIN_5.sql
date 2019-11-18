14c65e7e9034e3ffa9cd05fa46ae171dfc4cee28
Carolina de Souza Viana <carolina.wuu@gmail.com>
2019-11-17 22:40:15 -0300

    Add files via upload

---
+++ arquivos/CAROLINA_CONSULTA_JOIN_5.sql
@@ -0,0 +1,7 @@
+SELECT PT.idPontoTuristico, PT.NOME_PONTOTURISTICO, array_to_string(array_agg(distinct O.numero_linha),', ') AS "Linha Onibus" 
+FROM PONTOTURISTICO PT 
+LEFT OUTER JOIN OnibusPontoTuristico OPT ON (PT.idPontoTuristico = OPT.idPontoTuristico) 
+LEFT OUTER JOIN Onibus O ON (OPT.idonibus = O.idonibus) 
+WHERE PT.idpontoturistico NOT IN 
+(SELECT idpontoturistico FROM PONTOTURISTICOAVALIACAO GROUP BY idpontoTuristico HAVING AVG(NOTA) > 0) 
+GROUP BY PT.idPontoTuristico;
