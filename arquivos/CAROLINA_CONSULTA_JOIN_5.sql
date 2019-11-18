c54fdfc5dd59093dc2d4e04bba5811c8c46e9f58
Carolina de Souza Viana <carolina.wuu@gmail.com>
2019-11-17 22:52:28 -0300

    Update CAROLINA_CONSULTA_JOIN_5.sql

--- arquivos/CAROLINA_CONSULTA_JOIN_5.sql
+++ arquivos/CAROLINA_CONSULTA_JOIN_5.sql
@@ -4,4 +4,5 @@ LEFT OUTER JOIN OnibusPontoTuristico OPT ON (PT.idPontoTuristico = OPT.idPontoTu
 LEFT OUTER JOIN Onibus O ON (OPT.idonibus = O.idonibus) 
 WHERE PT.idpontoturistico NOT IN 
 (SELECT idpontoturistico FROM PONTOTURISTICOAVALIACAO GROUP BY idpontoTuristico HAVING AVG(NOTA) > 0) 
-GROUP BY PT.idPontoTuristico;
+GROUP BY PT.idPontoTuristico 
+ORDER BY PT.idPontoTuristico;
